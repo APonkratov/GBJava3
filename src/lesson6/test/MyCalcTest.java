c64026492b5985d62619b5551bdcf52e12fdc2fe
Aleksandr Ponkratov <aleks.ponkratov@gmail.com>
2020-07-07 14:24:08 +0300

    Lesson6

---
+++ src/lesson6/test/MyCalcTest.java
@@ -0,0 +1,78 @@
+package lesson6.test;
+
+import lesson6.src.testing.MyCalc;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.Timeout;
+
+import static java.lang.Thread.sleep;
+
+public class MyCalcTest {
+    private static MyCalc mycalc;
+
+    @BeforeClass
+    public static void initTest() {
+        mycalc = new MyCalc();
+        System.out.println("init suite");
+    }
+
+    @Test
+    public void testAdd() {
+        Assert.assertEquals(7, mycalc.add(3, 4));
+    }
+
+    @Test
+    public void testAdd02() {
+        Assert.assertEquals(6, mycalc.add(2, 4));
+    }
+
+    @Test
+    public void testAdd03() {
+        Assert.assertEquals(7, mycalc.add(3, 4));
+    }
+
+    @Test
+    public void testAdd04() {
+        Assert.assertEquals(7, mycalc.add(3, 4));
+    }
+
+    @Test
+    public void testAdd2() {
+        Assert.assertEquals(0, mycalc.add(0, 0));
+    }
+
+    @Test
+    public void testSub() {
+        Assert.assertEquals(6, mycalc.sub(9, 3));
+    }
+
+    @Test
+    public void testDiv() {
+        Assert.assertEquals(3, mycalc.div(9, 3));
+    }
+
+    @Test
+    public void testMul() {
+        Assert.assertEquals(28, mycalc.mul(9, 3));
+    }
+
+    @Test(expected = ArithmeticException.class)
+    public void testDivBy0() {
+        mycalc.div(10, 0);
+    }
+
+    @Rule
+    public final Timeout timeout = new Timeout(1000);
+
+    @Test(timeout=1000)
+    public void testVeryFastMethod() {
+        mycalc.add(10, 10);
+        try {
+            sleep(200);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
